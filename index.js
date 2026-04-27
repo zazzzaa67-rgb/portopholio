00e64ca1431799f55bbada1f204e5bb5c86cc5b7
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-04-27 16:00:10 +0300

    commit changes

---
+++ index.js
@@ -0,0 +1,16 @@
+const showbtn = document.getElementById("show-nav")
+const main = document.getElementById("main-part")
+const navigation = document.getElementById("nav-part")
+const links = document.querySelectorAll(".link")
+showbtn.addEventListener("click" , function(){
+    navigation.style.display = "flex" ;
+    main.style.display = "none"
+    showbtn.style.display = "none"
+})
+links.forEach(function(link) {
+    link.addEventListener("click", function () {
+        navigation.style.display = "none";
+        main.style.display = "flex";
+        showbtn.style.display = "flex";
+    });
+});
